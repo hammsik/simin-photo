20e2f97f43e2d05fe9a407cb18f477bb1d25f1a3
hammsik <asbbrp@gmail.com>
2025-04-30 03:48:54 +0900

    style: 쓸 데 없는 코드 제거

--- src/page/Intro.tsx
+++ src/page/Intro.tsx
@@ -1,12 +1,7 @@
 import { motion } from "motion/react";
-import { use } from "motion/react-client";
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 
-type CircleGeneratorProps = {
-  direction: "left" | "right";
-};
-
-const CircleGenerator = ({ direction }: CircleGeneratorProps) => {
+const CircleGenerator = () => {
   const [circleList, setCircleList] = useState<number[]>([]);
   const [screenWidth, setScreenWidth] = useState<number>(0);
 
@@ -26,7 +21,6 @@ const CircleGenerator = ({ direction }: CircleGeneratorProps) => {
   useEffect(() => {
     const interval = setInterval(() => {
       setCircleList((prev) => {
-        // 최대 3개의 원만 유지
         const newList = [...prev, (prev[prev.length - 1] ?? 0) + 1];
         if (newList.length > 20) {
           return newList.slice(-20); // 가장 최근 3개만 유지
@@ -34,15 +28,14 @@ const CircleGenerator = ({ direction }: CircleGeneratorProps) => {
         return newList;
       });
     }, 500);
-
     return () => clearInterval(interval);
-  }, [direction]); // direction이 변경될 때만 effect 재실행
+  }, []); // direction이 변경될 때만 effect 재실행
 
   return (
-    <div className={`w-full bg-black h-18 relative`}>
+    <div className={`w-full bg-black h-24 relative`}>
       {circleList.map((circle) => (
         <motion.div
-          className="absolute top-1/2 -translate-y-1/2 -right-8 rounded-xl bg-[#ECEAE9] h-10 w-8"
+          className="absolute top-1/2 -translate-y-1/2 -right-8 rounded-xl bg-[#ECEAE9] h-12 w-10"
           key={circle}
           animate={{ opacity: 1, x: -(screenWidth + 100) }}
           transition={{ duration: 8, ease: "linear" }}
@@ -55,7 +48,7 @@ const CircleGenerator = ({ direction }: CircleGeneratorProps) => {
 export const Intro = () => {
   return (
     <div className="size-full justify-between items-center flex flex-col">
-      <CircleGenerator direction="left" />
+      <CircleGenerator />
       <div className="flex flex-col justify-center items-center gap-4 w-full">
         <h1 className="font-play text-6xl font-stretch-50% text-[#B89A51] font-semibold">
           Simin Photo Booth
@@ -64,7 +57,7 @@ export const Intro = () => {
           시민의교회 청년부 포토부스에 오신 것을 환영합니다
         </p>
       </div>
-      <CircleGenerator direction="right" />
+      <CircleGenerator />
     </div>
   );
 };
